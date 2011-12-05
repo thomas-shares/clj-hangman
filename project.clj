66550c70f4b37f45f2c1515cd4d8001343b952b4
Thomas van der Veen <th.vanderveen@gmail.com>
2011-12-05 21:28:41 +0000

    first commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-hangman "1.0.0-SNAPSHOT"
+  :description "Clojure version of Hangman... not complete..."
+  :main  clj-hangman.core
+  :test-path "spec/"
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[swank-clojure "1.3.2" ] [speclj "1.2.0"]])
